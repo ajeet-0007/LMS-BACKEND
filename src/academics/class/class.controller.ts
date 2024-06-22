66b0a7f84798a962da90a09aad8edc09af7b672f
Apple <apple@Apples-MacBook-Air.local>
2024-06-23 01:44:03 +0530

    add class api added

--- src/academics/class/class.controller.ts
+++ src/academics/class/class.controller.ts
@@ -1,17 +1,55 @@
-import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { ClassService } from './class.service';
 import { StreamService } from '../stream/stream.service';
 import { SectionService } from '../section/section.service';
 
 @Controller('class')
 export class ClassController {
-  constructor(private readonly classService: ClassService, private readonly streamService: StreamService, private readonly sectionService: SectionService) {}
+  constructor(
+    private readonly classService: ClassService,
+    private readonly streamService: StreamService,
+    private readonly sectionService: SectionService,
+  ) {}
   @Get('/get-section-and-stream')
   async getClassSectionAndStream() {
     try {
-        const streams = await this.streamService.getStreamData();
-        const sections = await this.sectionService.getSectionData();
-        return {...streams, ...sections}
+      const streams = await this.streamService.getStreamData();
+      const sections = await this.sectionService.getSectionData();
+      return { ...streams, ...sections };
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Internal Server Error',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        { cause: error },
+      );
+    }
+  }
+
+  @Post('/add-class')
+  async addClass(@Body() body: any) {
+    try {
+      const result = await this.classService.addClass(
+        body.class_name,
+        body.class_section,
+        body.class_stream === null ? null : body.class_stream,
+      );
+      if (result.success && !result.dataFound)
+        return {
+          message: 'Record Inserted Successfully',
+          result: { success: result.success, section_id: result.classId },
+        };
+      else return { message: 'Record Already Exist', result };
     } catch (error) {
       console.log(error);
       throw new HttpException(
--- src/academics/class/class.service.ts
+++ src/academics/class/class.service.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/typeorm';
+import { Connection, IntegerType } from 'typeorm';
 
 @Injectable()
 export class ClassService {
+    constructor(@InjectConnection() private readonly connection: Connection) {}
+    async addClass(
+        className: string, classSection: string, classStream: string
+      ): Promise<{ success: boolean; dataFound: boolean; classId: Number }> {
+        // Call the stored procedure
+        const [results] = await this.connection.query('CALL AddClass(?, ?, ?)', [
+          className, classSection, classStream
+        ]);
     
+        // Extract the success flag from the results
+        const success = results[0]?.success;
+        const dataFound = results[0]?.data_found;
+        const classId = results[0]?.class_id;
+        return { success: !!success, dataFound: !!dataFound, classId: classId };
+      }
 }
